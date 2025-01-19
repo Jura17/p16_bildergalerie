7c18fca33f9b3a1e4163566271cf73e6fafdcddb
Julian Rakow <julian.rakow@yahoo.com>
2025-01-19 20:21:13 +0100

    Add asset structure and images in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyGallery"),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              Image(image: AssetImage("assets/images/bike.jpeg")),
+            ],
+          ),
         ),
       ),
     );
