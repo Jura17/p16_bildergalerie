da1ba729eab943ffc0577075f0fd2bec4343c1b6
Julian Rakow <julian.rakow@yahoo.com>
2025-01-21 11:38:28 +0100

    basic structure, navigation in place

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:p16_bildergalerie/about_me_screen.dart';
+import 'package:p16_bildergalerie/gallery_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,18 +12,48 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MyGallery"),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Image(image: AssetImage("assets/images/bike.jpeg")),
-            ],
-          ),
-        ),
+      home: GalleryApp(),
+    );
+  }
+}
+
+class GalleryApp extends StatefulWidget {
+  const GalleryApp({super.key});
+
+  @override
+  State<GalleryApp> createState() => _GalleryAppState();
+}
+
+class _GalleryAppState extends State<GalleryApp> {
+  int activeIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyGallery"),
       ),
+      body: [
+        GalleryScreen(),
+        AboutMeScreen(),
+      ][activeIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: activeIndex,
+          onTap: (index) {
+            setState(() {
+              activeIndex = index;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.image),
+              label: "Bilder",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Über mich",
+            ),
+          ]),
     );
   }
 }
